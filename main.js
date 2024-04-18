6f87cc5aa1c957b56d6059f3c15ee928913ed1a9
hertheyhermee <tiamiyuhayzed@gmail.com>
2024-04-18 17:42:35 +0100

    updates

--- main.js
+++ main.js
@@ -1,8 +1,36 @@
 const hamburgerMajor = document.querySelector('.ham-major-wrapper');
 const hamMajItem = document.querySelector('.ham-major-item');
+const sideBarOne = document.querySelector('.left-sidebar');
+const hamburger = document.querySelector('.hamburger');
+const sideBarTwo = document.querySelector('.right-sidebar')
+const cancel = document.querySelector(".close");
+const overlay = document.querySelector('.overlay')
+const wrapper = document.querySelector('.wrapss')
+console.log(cancel);
+
+hamburger.addEventListener('click', () => {
+    sideBarTwo.classList.add('active')
+})
+
+cancel.addEventListener('click', () => {
+    sideBarTwo.classList.remove('active');
+})
+console.log(window);
+function closeSidebars(item) {
+    wrapper.addEventListener('click', () => {
+        item.classList.remove('active');
+    })
+}
 
 hamburgerMajor.addEventListener('click', () => {
     hamMajItem.classList.toggle('onclick');
+    if (hamMajItem.classList.contains('onclick')) {
+        sideBarOne.classList.add('active')
+        overlay.classList.add('active')
+    } else  {
+        sideBarOne.classList.remove('active')
+        overlay.classList.remove('active')
+    }
 })
 
 const headers = document.querySelectorAll('.header');
